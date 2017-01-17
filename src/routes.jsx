889f8c0d04053bc3fc27c847a7fe3fd524a92c44
Wild East Engineer <wildeastengineer@gmail.com>
2017-01-17 08:08:31 +0400

    HelloWorld. Remove axtra components

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,20 +7,11 @@ import CategoriesPage from 'components/CategoriesPage';
 import CurrenciesPage from 'components/CurrenciesPage';
 import DashboardPage from 'components/DashboardPage';
 
-import CounterPage from 'components/CounterPage';
-import HelloWorldPage from 'components/HelloWorldPage';
-import TimePage from 'components/TimePage';
-
 export default (
     <Route component={App} path='/'>
         <IndexRoute component={DashboardPage}/>
-
         <Route component={AccountsPage} path='settings/accounts'/>
         <Route component={CategoriesPage} path='settings/categories'/>
         <Route component={CurrenciesPage} path='settings/currencies'/>
-
-        <Route component={CounterPage} path='counters'/>
-        <Route component={HelloWorldPage} path='hello'/>
-        <Route component={TimePage} path='time'/>
     </Route>
 );
